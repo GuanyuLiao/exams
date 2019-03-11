fd0e9b1af89d4d117859b141a1ad420bf3d5e5c1
liang <1005252998@qq.com>
2019-03-11 19:46:50 +0800

    Signed-off-by: liang <1005252998@qq.com>

---
+++ exams/p1980.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+
+int count(int count_num, int base_num);
+
+int p1980(void) {
+	int n, x;
+	int _count = 0;
+
+	scanf("%d %d", &n, &x);
+	for (int i = 1; i <= n; i++) {
+		_count += count(i, x);
+	}
+	printf("%d", _count);
+
+	system("pause");
+	return 0;
+}
+
+int count(int count_num, int base_num) {
+	if (count_num == 0) {
+		return 0;
+	}
+	else if (count_num % 10 == base_num) {
+		return 1 + count(count_num / 10, base_num);
+	}
+	else {
+		return count(count_num / 10, base_num);
+	}
+}
